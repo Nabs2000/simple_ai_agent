c489fe73b749f5fda671c0eb6ab9da55af9fb493
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-04-20 19:24:25 -0700

    Implement LangGraph

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-import os
-from dotenv import load_dotenv
-from langchain.agents import initialize_agent, Tool
-from langchain_openai import OpenAI
+from langchain_core.tools import tool
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 from langchain_community.tools import DuckDuckGoSearchRun
+from langgraph.prebuilt import create_react_agent
+import getpass
+import os
 
-load_dotenv()
-openai_api_key = os.getenv("OPENAI_API_KEY")
-llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
-search = DuckDuckGoSearchRun()
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("OpenAI API key:\n")
 
-tools = [
-    Tool(
-        name="Search",
-        func=search.run,
-        description="Useful for answering questions about current events."
-    )
-]
+model = ChatOpenAI(model="gpt-4o")
 
-agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+@tool
+def search(input: str) -> str:
+    """
+    Use this tool to search for information on the internet.
+    """
+    search = DuckDuckGoSearchRun()
+    return search.run(input)
 
-response = agent.run("Who is the current president of the United States?")
-print(response)
+tools = [search]
+
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "You are a helpful assistant."),
+#     ("user", "{input}"),
+#     ("placeholder", "{agent_scratchpad}"),
+# ])
+
+agent = create_react_agent(model, tools)
+
+query = input("Enter your query: ")
+
+messages = agent.invoke({"messages": [("human", query)]})
+print(messages["messages"][-1].content)
